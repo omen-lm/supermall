db9881e9862a66e678449a0e50734b3b9149cc87
omen_lm <1139219070@qq.com>
2020-03-10 08:12:45 +0800

    模块化封装

--- src/network/request.js
+++ src/network/request.js
@@ -1,18 +1,25 @@
 import axios from 'axios'
 export function request(config){
-  return new Promise((resolve,reject)=>{
-    const instance = axios.create({
-      baseURL:'http://123.207.32.32:8000',
-      timeout:5000
-    })
-
-    return instance(config)
-    // instance(config)
-    // .then(res =>{
-    //   resolve(res)
-    // })
-    // .catch(err =>{
-    //   reject(err)
-    // })
+  //1.创建axios的实例
+  const instance = axios.create({
+    baseURL:'http://123.207.32.32:8000',
+    timeout:5000
   })
+
+  //2.axios的拦截器
+  //2.1请求拦截
+  instance.interceptors.request.use(config=>{
+    return config
+  },err=>{
+
+  })
+  //2.2响应拦截
+  instance.interceptors.response.use(res=>{
+    return res.data
+  },err=>{
+    console.log(err);
+  })
+
+  //3.发送真正的网络请求
+  return instance(config)
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,24 +1,173 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+    <ul>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
 import NavBar from '../../components/common/navbar/NavBar'
+import TabControl from '../../components/content/tabControl/TabControl'
+
+import {getHomeMultidata} from '../../network/home'
+
+
 export default {
   name: 'Home',
   components:{
-    NavBar
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl
+  },
+  data(){
+    return{
+      banners:[],
+      recommends:[],
+      goods:{
+        'pop':{page:0,list:[]},
+        'news':{page:0,list:[]},
+        'sell':{page:0,list:[]},
+      }
+    }
+  },
+  created(){
+    //1.请求多个数据
+    getHomeMultidata().then(res=>{
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
   }
 }
 </script>
 
 <style>
+  #home{
+    padding-top: 44px;
+  }
+
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
+    position:fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    z-index: 9;
+  }
+
+  .tab-control{
+    position: sticky;
+    top: 44px;
   }
 </style>
